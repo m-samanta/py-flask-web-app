2964c856a3ecb6932aadb227a903ce582bc5e9cb
Mriganka Samanta <m.samanta.dev@gmail.com>
2024-06-17 19:43:31 -0400

    Waitress pip

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from weather import get_current_weather
+from waitress import serve
 
 app = Flask(__name__)
 
@@ -9,4 +10,4 @@ def index():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    serve(app, host="0.0.0.0", port=8000)
